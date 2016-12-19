d10c567d6e0f600f34455e3f7bfbe6fbe76a70cc
Victor Barrios Jimenez <vhbj149601@gmail.com>
2016-12-18 22:12:48 -0600

    deleted some items

--- Chapter_05/05_02/JS/sidebar-switcher.js
+++ Chapter_05/05_02/JS/sidebar-switcher.js
@@ -1,3 +1,12 @@
 ( function( $ ) { 
-    
-})( jQuery );
+    $('a.sidebar-left-toggle').click(function(){
+        if (!$('sidebar').hasClass('sidebar-left')){
+            $('.sidebar').addClass('sidebar-left');
+        };
+    });
+    $('a.sidebar-right-toggle').click(function(){
+        if ($('sidebar').hasClass('sidebar-left')){
+            $('.sidebar').removeClass('sidebar-left');
+        };
+    });  
+})(jQuery);
